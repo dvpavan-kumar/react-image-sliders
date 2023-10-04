3c604bbf70d6d8c84b1fddf8809034e70d82a496
Pavan.D <dpavan.vkumar@gmail.com>
2023-10-05 01:08:23 +0530

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import image1 from "./assets/images/city.jpg";
+import image2 from "./assets/images/galaxy.jpg";
+import image3 from "./assets/images/gregory.jpg";
+import image4 from "./assets/images/iceview.jpeg";
+import image5 from "./assets/images/mountains.jpg";
+import image6 from "./assets/images/nature.jpg";
+import image7 from "./assets/images/space.jpg";
+import "./App.css";
+import { ImageSlider } from "./ImageSlider";
 
-function App() {
-  const [count, setCount] = useState(0)
+const IMAGES = [
+  { url: image1, alt: "city" },
+  { url: image2, alt: "galaxy" },
+  { url: image3, alt: "gregory" },
+  { url: image4, alt: "iceview" },
+  { url: image5, alt: "mountains" },
+  { url: image6, alt: "nature" },
+  { url: image7, alt: "space" },
+];
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        maxWidth: "1200px",
+        width: "100%",
+        aspectRatio: "10 / 4",
+        margin: "0 auto",
+      }}
+    >
+      <h1>Image Slider</h1>
+      <ImageSlider images={IMAGES} />
+      <a href="/" style={{ fontSize: "3rem" }}>
+        link
+      </a>
+    </div>
+  );
 }
-
-export default App
